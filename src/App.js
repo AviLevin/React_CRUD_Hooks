7388d8bb480d92c52c1951bebd0e000fb514e589
avremi89 <avremi89@gmail.com>
2021-11-23 22:16:53 +0200

    s
    s

--- src/App.js
+++ src/App.js
@@ -1,30 +1,87 @@
-import React, {useState} from 'react';
-import UserTable from './tables/UserTable';
-
+import React, { useState } from "react";
+import UserTable from "./tables/UserTable";
+import AddUserForm from "./forms/AddUserForm";
+import EditUserForm from "./forms/EditUserForm";
 const App = () => {
-
   const usersData = [
-    { id: 1, name: 'Tania', username: 'floppydiskette' },
-    { id: 2, name: 'Craig', username: 'siliconeidolon' },
-    { id: 3, name: 'Ben', username: 'benisphere' },
-  ]
+    { id: 1, name: "Avraham", username: "Avi" },
+    { id: 2, name: "Isaac", username: "Aizic" },
+    { id: 3, name: "Jacob", username: "Jake" },
+  ];
 
-  const [users, setUsers] = useState(usersData)
+  const [users, setUsers] = useState(usersData);
+  const [editing, setEditing] = useState(false);
+
+  const initialFormState = { id: null, name: "", username: "" };
+  const [currentUser, setCurrentUser] = useState(initialFormState);
+
+  const addUser = (user) => {
+    user.id = users.length + 1;
+    setUsers([...users, user]);
+    console.log("newIs", user);
+  };
+
+  const deleteUser = (id) => {
+    setUsers(users.filter((user) => user.id !== id));
+    setEditing(false);
+  };
+
+  //update__________________________________________
+
+  const editRow = (user) => {
+    setEditing(true);
+
+    setCurrentUser({ id: user.id, name: user.name, username: user.username });
+  };
+
+  const updateUser = (id, updatedUser) => {
+    setEditing(false);
+
+    setUsers(users.map((user) => (user.id === id ? updatedUser : user)));
+    console.log("updated");
+  };
+
+  //</>_______________________________________________
 
   return (
     <div className="container">
-      <h1>CRUD App with Hooks</h1>
-      <div className="flex-row">
-        <div className="flex-large">
-          <h2>Add user</h2>
-        </div>
-        <div className="flex-large">
-          <h2>View users</h2>
-          <UserTable users={users}></UserTable>
+<div className="row top"><h1>CRUD App with Hooks</h1></div>
+
+      <div className="row">
+        <div className="col-3">
+          
+         
+              {editing ? (
+                <div>
+                  <h2>Edit user</h2>
+                  <EditUserForm
+                    setEditing={setEditing}
+                    currentUser={currentUser}
+                    updateUser={updateUser}
+                  />
+                </div>
+              ) : (
+                <div>
+                  <h2>Add user</h2>
+                  <AddUserForm addUser={addUser} />
+                </div>
+              )}
+            </div>
+          
+          <div className="col-9">
+          
+              <h2>View users</h2>
+              <UserTable
+                users={users}
+                editRow={editRow}
+                deleteUser={deleteUser}
+              ></UserTable>
+            </div>
+          
         </div>
       </div>
-    </div>
-  )
-}
+    
+  );
+};
 
-export default App
+export default App;
